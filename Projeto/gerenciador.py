bf37ad166cd66425eaffba05834a6e98f6f66064
Jackez01 <lucasgefreitas@gmail.com>
2024-04-25 15:33:40 -0300

    deletar tarefas completadas

--- Projeto/gerenciador.py
+++ Projeto/gerenciador.py
@@ -29,6 +29,13 @@ def completar_tarefa(tarefas, indice_tarefa):
   print(f"Tarefa {indice_tarefa} marcada como completada")
   return
 
+def deletar_tarefas_completadas(tarefas):
+  for tarefa in tarefas:
+    if tarefa["completada"]:
+      tarefas.remove(tarefa)
+  print("Tarefas completadas foram deletadas.")
+  return
+
 tarefas = []
 while True:
   print("\n Menu do gerenciador de lista de tarefas:")
@@ -53,10 +60,15 @@ while True:
     indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
     novo_nome = input("Digite o novo nome da tarefa: ")
     atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+
   elif escolha == "4":
     ver_tarefas(tarefas)
     indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
     completar_tarefa(tarefas, indice_tarefa)
+
+  elif escolha == "5":
+    deletar_tarefas_completadas(tarefas)
+    ver_tarefas(tarefas)
 
   elif escolha == "6":
     break
